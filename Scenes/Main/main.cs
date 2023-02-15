3facad328d51fbe2943368a53ed07852e87d1746
ThomasUijlen <thomasuijlen@hotmail.com>
2023-02-15 02:24:03 +0100

    Duplicate texture detection

--- Scenes/Main/main.cs
+++ Scenes/Main/main.cs
@@ -1,17 +1,23 @@
 using Godot;
 using System;
 using VoxelTerrainPlugin;
+using System.Collections.Generic;
 
 public partial class main : Sprite2D
 {
+	BlockLibrary blockLibrary = new BlockLibrary();
+	RandomNumberGenerator rng = new RandomNumberGenerator();
 	public override void _Ready()
 	{
-		BlockLibrary blockLibrary = new BlockLibrary();
-
-		blockLibrary.AddBlockType(Image.LoadFromFile("res://Stone.png"), new Color(1,1,1));
-		blockLibrary.AddBlockType(Image.LoadFromFile("res://Stone.png"), new Color(1,1,0));
-		blockLibrary.AddBlockType(Image.LoadFromFile("res://Stone.png"), new Color(1,0,1));
-		blockLibrary.AddBlockType(Image.LoadFromFile("res://Stone.png"), new Color(0,1,1));
+		blockLibrary.AddBlockType(new BlockType(Image.LoadFromFile("res://Textures/Stone.png"), new Color(1,1,1)));
+		blockLibrary.AddBlockType(new BlockType(Image.LoadFromFile("res://Textures/Dirt.png"), new Color(1,1,1)));
+		blockLibrary.AddBlockType(new BlockType(Image.LoadFromFile("res://Textures/Leaves.png"), new Color(1,1,1)));
+		blockLibrary.AddBlockType(new BlockType(
+			new Dictionary<BlockType.SIDE, Image>() {
+				{BlockType.SIDE.DEFAULT, Image.LoadFromFile("res://Textures/Logs.png")},
+				{BlockType.SIDE.TOP, Image.LoadFromFile("res://Textures/Wood.png")},
+				{BlockType.SIDE.BOTTOM, Image.LoadFromFile("res://Textures/Wood.png")}
+				}, new Color(1,1,1)));
 
 		Texture = ImageTexture.CreateFromImage(blockLibrary.textureAtlas);
 	}
