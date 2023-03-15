317f140e86899bd6133825217ceb53ab2eb42f44
ThomasUijlen <thomasuijlen@hotmail.com>
2023-03-16 00:05:41 +0100

    Simple brush

--- Assets/Editor/CollisionGrid/collision_grid.gd
+++ Assets/Editor/CollisionGrid/collision_grid.gd
@@ -1,8 +1,6 @@
 extends Node3D
 
-@export var boxSize = 20
-
-var boxes : Dictionary = {}
+@export var boxSize : Vector3
 
 var api : VoxelAPI
 
@@ -16,17 +14,16 @@ func _ready():
 	
 	var half = boxSize/2
 	
-	for x in range(boxSize):
-		for y in range(boxSize):
-			for z in range(boxSize):
-				var coord = Vector3(x,y,z)-Vector3.ONE*half
+	for x in range(boxSize.x):
+		for y in range(boxSize.y):
+			for z in range(boxSize.z):
+				var coord = Vector3(x-half.x,y-half.y,z-half.z)
 				var newBlock = block.duplicate()
 				newBlock.position = coord
 				add_child(newBlock)
-				boxes[coord] = newBlock
 	
 	block.queue_free()
 
 func _process(delta):
-	var cameraPos = get_viewport().get_camera_3d().global_position.round()
+	var cameraPos = get_viewport().get_camera_3d().global_position.floor()
 	global_position = cameraPos
--- Scenes/Main/VoxelAPI.gd
+++ Scenes/Main/VoxelAPI.gd
@@ -10,13 +10,16 @@ func _ready():
 	camera = get_viewport().get_camera_3d()
 
 var changed : bool = false
+var changedTimer : float = 0.0
 func _process(delta):
 	var currentCamPos = camera.global_position.floor()
 	if previousCamPos != currentCamPos:
 		changed = true
 		previousCamPos = currentCamPos
 	
-	if changed:
+	changedTimer -= delta
+	if changed and changedTimer <= 0.0:
+		changedTimer = 0.2
 		changed = false
 		emit_signal("worldChanged")
 
